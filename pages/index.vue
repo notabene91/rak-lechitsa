521d58a855f27abb8cc912343575944f2ccd5f66
Egor Levchenko <antytoto@gmail.com>
2020-05-01 12:54:22 +0300

    created vue

--- pages/index.vue
+++ pages/index.vue
@@ -9,11 +9,7 @@
         A project for Khabensky Foundation
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
           Documentation
         </a>
         <a
@@ -29,13 +25,13 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from '~/components/Logo.vue';
 
 export default {
   components: {
-    Logo
-  }
-}
+    Logo,
+  },
+};
 </script>
 
 <style>
