65bd8683fc3a4685b3c6f855b6c93118c4b229ad
Egor Levchenko <antytoto@gmail.com>
2020-05-06 17:41:26 +0300

    created header vithout menu

---
+++ components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <header class="header">
+    <div class="header__container container">
+      <div class="header__title strong">
+        Проект благотворительного фонда<br />
+        Константина Хабенского
+      </div>
+      <!-- это левый блок-->
+      <nav class="header__nav">
+        <!-- вот сюда будет подключаться главное меню, на место div's-->
+        <div></div>
+        <!-- вот на эту ссылку должен вешаться обработчик, который будет вызывать попап -->
+        <span class="header__login">
+          Рассказать историю
+        </span>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.header {
+  padding: 0 18px;
+}
+.header__container {
+  margin: 0 auto;
+
+  display: -webkit-flex;
+  display: -webkit-box;
+  display: -moz-flex;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -ms-flex;
+  display: -o-flex;
+  display: flex;
+  -webkit-box-pack: justify;
+  -webkit-justify-content: space-between;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  -webkit-box-align: center;
+  -webkit-align-items: center;
+  -ms-flex-align: center;
+  align-items: center;
+}
+.header__login {
+  cursor: pointer;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
+    <i-header />
     <nuxt />
   </div>
 </template>
 
 <script>
-export default {};
+import Header from '@/components/Header';
+export default {
+  components: {
+    'i-header': Header,
+  },
+};
 </script>
 
-<style scoped></style>
+<style>
+/* blocks */
+html {
+  background: white;
+  color: black;
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 24px;
+}
+.container {
+  width: 100%;
+  max-width: 1320px;
+}
+/* modificators */
+.strong {
+  font-weight: 600;
+}
+</style>
