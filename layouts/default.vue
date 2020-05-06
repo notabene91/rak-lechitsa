b9fd8fe2cfb8e71b5d37b7fc41d339407e385e70
Egor Levchenko <antytoto@gmail.com>
2020-05-06 18:48:21 +0300

    created buttons style

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,9 +26,105 @@ html {
   line-height: 24px;
 }
 .container {
-  width: 100%;
   max-width: 1320px;
 }
+
+/* elements */
+.caption {
+  margin-block-start: 0px;
+  margin-block-end: 0px;
+}
+.caption_h1 {
+  font-weight: 800;
+  font-size: 92px;
+  line-height: 111px;
+  text-align: center;
+}
+.caption_h2 {
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 36px;
+}
+
+.btn,
+.btn:visited {
+  padding: 17px 44px;
+  background: #613a93;
+  color: white;
+  cursor: pointer;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: center;
+  text-decoration: none;
+}
+.btn:hover {
+  background: #613a93;
+  opacity: 0.9;
+}
+.large-btn,
+.large-btn:hover,
+.large-btn:visited {
+  display: -webkit-flex;
+  display: -webkit-box;
+  display: -moz-flex;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -ms-flex;
+  display: -o-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -webkit-align-items: center;
+  -ms-flex-align: center;
+  align-items: center;
+  cursor: pointer;
+  background: #fbfbfb;
+  text-decoration: none;
+  padding: 31px 0;
+}
+.num-btn,
+.num-btn:hover .num-btn:visited {
+  width: 58px;
+  display: -webkit-flex;
+  display: -webkit-box;
+  display: -moz-flex;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -ms-flex;
+  display: -o-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -webkit-align-items: center;
+  -ms-flex-align: center;
+  align-items: center;
+  cursor: pointer;
+  background: #fbfbfb;
+  text-decoration: none;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+  padding: 20px 0;
+}
+
+.num-btn.num-btn_active {
+  background: #f4f4f4;
+}
+
+.link,
+.link:hover,
+.link:visited {
+  color: inherit;
+  text-decoration: none;
+}
+
 /* modificators */
 .strong {
   font-weight: 600;
