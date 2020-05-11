f1eee2e08ef19a017d8293801bef9c45f0ecc4c8
notabene91 <notabene1991@yandex.ru>
2020-05-11 16:19:36 +0300

    create section Tell

---
+++ components/Tell.vue
@@ -0,0 +1,75 @@
+<template>
+  <section class="tell">
+    <h2 class="tell__title">Расскажите свою историю</h2>
+    <div class="tell__container">
+      <p class="tell__text">
+        Мы публикуем новые истории на сайте раз в неделю. Есть 2 варианта
+        поделиться своей историей неизлечимых привычек, навязчивых идей и
+        болезненных привязанностей.
+      </p>
+
+      <div class="tell__variants">
+        <p class="tell__var tell__var_active">1-й вариант</p>
+        <p class="tell__var">2-й вариант</p>
+      </div>
+
+      <div class="tell__instruction">
+        <p class="tell__instruction-text">
+          Заполнить подробную форму прямо на сайте и мы опубликуем вашу историю
+          после проверки. Пожалуйста, заполняйте все пункты корректно, если вы
+          испытаете какие-то сложности, воспользуйтесь 2-м вариантом.
+        </p>
+        <button class="btn tell__btn">Заполнить форму</button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.tell {
+  padding: 100px 60px;
+  background-color: #f7f7f7;
+}
+
+.tell__container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.tell__title {
+  margin: 0 0 32px 0;
+  max-width: 340px;
+  font-size: 32px;
+  line-height: 36px;
+}
+
+.tell__text {
+  margin: 0 192px 0 0;
+  max-width: 340px;
+  color: #666;
+  line-height: 22px;
+}
+
+.tell__var {
+  margin: 0 0 10px;
+  color: #a2a2a2;
+}
+
+.tell__var_active {
+  color: #000;
+}
+
+.tell__instruction {
+  max-width: 640px;
+  margin-left: 40px;
+}
+
+.tell__instruction-text {
+  margin: 0 0 72px;
+  color: #666;
+}
+</style>
