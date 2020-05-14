7b496e3709963cd7b18815cf75694f23c7ca871a
Damir Vakhitov <damirvakhitov@gmail.com>
2020-05-14 14:30:24 +0300

    Класс container для всех разрешений. Вынес в ui кнопки arrow

---
+++ components/ui/ArrowButtonRigth.vue
@@ -0,0 +1,31 @@
+<template>
+  <picture class="arrow__pic">
+    <source srcset="/img/arrow--horizontal-r.svg" type="image/svg+xml" />
+    <img
+      src="/img/arrow--horizontal-r.png"
+      srcset="
+        /img/arrow--horizontal-r@2x.png 2x,
+        /img/arrow--horizontal-r.png    1x
+      "
+      alt="показать следующее"
+      class="arrow"
+    />
+  </picture>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.arrow_rotate {
+  /* зеркалим изображение */
+  transform: scale(-1, -1);
+}
+
+.arrow_inactive {
+  /* делаем полупрозрачным */
+  filter: alpha(Opacity=25);
+  opacity: 0.25;
+}
+</style>
